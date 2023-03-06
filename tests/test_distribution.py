ab7c57fef701aef8fe0c22a4f50d1a8bd87777cb
Henry Schreiner <HenrySchreinerIII@gmail.com>
2023-03-06 18:57:53 -0500

    Update test_distribution.py

--- tests/test_distribution.py
+++ tests/test_distribution.py
@@ -10,6 +10,7 @@ DIR = Path(__file__).parent.resolve()
 def test_source_distribution(virtualenv, tmp_path):
     sdist_dir = tmp_path / "dist"
     workspace = str(tmp_path / "workspace")
+    workspace.mkdir()
     virtualenv.install("build")
     virtualenv.module("build", "--sdist", "--outdir", sdist_dir, cwd=DIR.parent)
     (sdist,) = sdist_dir.glob("*.tar.gz")
@@ -25,6 +26,7 @@ def test_source_distribution(virtualenv, tmp_path):
 def test_wheel(virtualenv, tmp_path):
     wheel_dir = tmp_path / "dist"
     workspace = str(tmp_path / "workspace")
+    workspace.mkdir()
     virtualenv.install("build")
     virtualenv.module("build", "--wheel", "--outdir", wheel_dir, cwd=DIR.parent)
     (wheel,) = wheel_dir.glob("*.whl")
